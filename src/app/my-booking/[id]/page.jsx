8d77cb461dd95260d69eb56b45371faa581f722e
Raihan Uddin <rkrazzkhan01731@gmail.com>
2025-08-07 01:01:44 +0800

    api fetch completed

--- src/app/my-booking/[id]/page.jsx
+++ src/app/my-booking/[id]/page.jsx
@@ -1,6 +1,10 @@
 import CheckoutFormUpdate from "@/components/checkoutForm/CheckoutFormUpdate"
 
-const UpdateBookingPage = ({params}) => {
+const UpdateBookingPage = async ({params}) => {
+
+    const p = await params;
+    const res = await fetch(`http://localhost:3000/api/my-bookings/${p.id}`);
+    const data = await res.json()
   return <div>
     <CheckoutFormUpdate />
   </div>
